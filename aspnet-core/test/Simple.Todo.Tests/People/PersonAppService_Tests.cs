82c99e1fa1e51935528d6971cdcba2dbf50e082f
Lindelwa Mpetshwa <LMpets427@ignitiongroup.co.za>
2020-07-14 16:51:47 +0200

    updated unit test

---
+++ aspnet-core/test/Simple.Todo.Tests/People/PersonAppService_Tests.cs
@@ -0,0 +1,35 @@
+﻿using Simple.Todo.Tasks;
+using Simple.Todo.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Simple.Todo.Tasks.Dtos;
+using Shouldly;
+using Microsoft.EntityFrameworkCore;
+using Simple.Todo.People;
+
+namespace Simple.Todo.Tests.Tasks
+{
+    public class PersonAppService_Tests : TodoTestBase
+    {
+        private readonly IPersonAppService _personAppService;
+
+        public PersonAppService_Tests()
+        {
+            _personAppService = Resolve<IPersonAppService>();
+        }
+
+        [Fact]
+        public async void GetPeople_Test()
+        {
+            // Act
+            var output = await _personAppService.GetAllPeople();
+
+            // Assert
+            output.People.Count.ShouldBeGreaterThan(0);
+        }
+        
+        
+    }
+}
--- aspnet-core/test/Simple.Todo.Tests/Tasks/TasksAppService_Tests.cs
+++ aspnet-core/test/Simple.Todo.Tests/Tasks/TasksAppService_Tests.cs
@@ -6,7 +6,7 @@ using System.Text;
 using Xunit;
 using Simple.Todo.Tasks.Dtos;
 using Shouldly;
-
+using Microsoft.EntityFrameworkCore;
 namespace Simple.Todo.Tests.Tasks
 {
     public class TasksAppService_Tests : TodoTestBase
@@ -28,5 +28,39 @@ namespace Simple.Todo.Tests.Tasks
             output.Tasks.Count.ShouldBeGreaterThan(0);
         }
         
+        [Fact]
+        public async void CreateTask_Test()
+        {
+            // Act
+             _taskAppService.CreateTask(
+                new CreateTaskInput
+                {
+                    Description="Review Pull Request",
+                    AssignedPersonId=1,
+                });
+            // Assert
+            await UsingDbContextAsync(async context =>
+            {
+                var taskDetails = await context.Task.FirstOrDefaultAsync(i=>i.Description == "Review Pull Request" && i.AssignedPersonId==1);
+                taskDetails.ShouldNotBeNull();
+            });
+        }
+        [Fact]
+        public async void UpdateTask_Test()
+        {
+            // Act
+            _taskAppService.UpdateTask(
+              new UpdateTaskInput { 
+              TaskId=1,
+               State=TaskState.Completed
+              });
+
+            // Assert
+            await UsingDbContextAsync(async context =>
+            {
+                var taskDetails = await context.Task.FirstOrDefaultAsync(i => i.Id ==1 && i.State == TaskState.Completed);
+                taskDetails.ShouldNotBeNull();
+            });
+        }
     }
 }
